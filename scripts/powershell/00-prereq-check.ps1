c69828a31743c481c65a9eb3eb9c215c08addbfd
devops <arudrasunkari@gmail.com>
2026-05-30 20:07:59 +0530

    Refresh git tracking

--- scripts/powershell/00-prereq-check.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$tools = @("aws", "terraform", "kubectl", "helm", "docker", "git")
-foreach ($tool in $tools) {
-    Write-Host "Checking $tool..."
-    & $tool --version
-}
-
-Write-Host "All basic tools are available."
--- scripts/powershell/07-deploy-argocd-app.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-kubectl apply -f gitops/argocd/order-api-dev-application.yaml
-
-kubectl get applications -n argocd
-kubectl get pods -n ekslambda-dev
