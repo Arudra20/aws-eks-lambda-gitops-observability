ba1fac1237d8459e0dd0a0ee2e744d720b2160cc
devops <arudrasunkari@gmail.com>
2026-05-30 20:49:21 +0530

    files added terraform backend.tf

--- infra/terraform/envs/dev/backend.tf
+++ infra/terraform/envs/dev/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "eks_lambda"
+    key            = "ekslambda/dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "ekslambda-dev-tf-locks"
+    encrypt        = true
+  }
 }
