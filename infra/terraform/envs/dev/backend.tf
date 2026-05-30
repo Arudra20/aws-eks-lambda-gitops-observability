bd09c69666ac5ba5d0f8deca716ee0269b8d2902
devops <arudrasunkari@gmail.com>
2026-05-31 01:45:38 +0530

    infra updated

--- infra/terraform/envs/dev/backend.tf
+++ infra/terraform/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ekslambda-gitops-dev"
+    bucket         = "eks-lambda-gitops-dev"
     key            = "ekslambda/dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "ekslambda-dev-tf-locks"
